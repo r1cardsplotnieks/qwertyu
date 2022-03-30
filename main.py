cc288e8ee2ffcc1676e2792900096e24be9d1375
RIČARDS PLOTNIEKS <100768873+r1cardsplotnieks@users.noreply.github.com>
2022-03-30 09:20:19 +0000

    qwerty

--- main.py
+++ main.py
@@ -1,23 +1,16 @@
 import random
 import csv
-
 CRED = '\033[91m'
 CEND = '\033[0m'
 D = '\033[36m'
-def janis():
-  print("Spēles nosaukums - ", CRED + "Slapjie bandīti" + CEND)
-  print("Autori - Ričards Plotnieks, Rainers Jānis Sprincis")
-
-janis()
-
 def nokrisni():
   nejaus = random.randint(1, 2)
   if nejaus == 1:
     cela_garums = 80
-    print("Pašreiz ārā līst lietus. Var nobraukt 80km.")
+    print("Pašreiz ārā līst lietus.")
   else:
     cela_garums = 140
-    print("Pašreiz ārā spīd saule. Var nobraukt 140km.")
+    print("Pašreiz ārā spīd saule.")
   return cela_garums
 
 def sleep():
@@ -31,8 +24,12 @@ def noteikumi():
         print(rinda['noteikumi'])
     noteikumi1.close()
 
-
-
+def ievads():
+  print("Spēles nosaukums - ", CRED + "Slapjie bandīti" + CEND)
+  print("Autori - Ričards Plotnieks, Rainers Jānis Sprincis")
+  print()
+  noteikumi()
+ievads()
 def laiks():
   import time
   seconds = time.time()
@@ -40,9 +37,9 @@ def laiks():
   print(CRED + "Laiks, kad sākāt spēli:" + CEND, local_time)
 
 laiks()
-
-
 def valmiera():
+  print("1. kontrolpunkts - Svētā Sīmaņu baznīca.")
+  sleep()
   punkti = 0
   skaits = 1
   jautajumi1 = open("jautajumi.csv", "r")
@@ -70,8 +67,8 @@ def valmiera():
   lasa1 = csv.DictReader(x1)
   for rinda in lasa1:
     y = nokrisni() - int(rinda['k1'])
+    print("Nākamais kontrolpunkts - Cēsis,",str(rinda['k1'])+"km attālumā.")
   print("Jūs vēl varat nobraukt:", str(y) + "km.")
-  print("Nākamais kontrolpunkts - Cēsis!")
   sleep()
   jautajumi1.close()
   return y
@@ -79,19 +76,22 @@ def valmiera():
 
 
 def cesis():
+  print("2. kontrolpunkts - Cēsu pils.")
+  sleep()
   a = valmiera()
+  print("Jūs esat nonākuši Cēsu pilī.")
   x1 = open("kontrolpunkti.csv", "r")
   lasa1 = csv.DictReader(x1)
   for rinda in lasa1:
     z = int(rinda['k2'])
-    if (a - z) < 0:
-      print()
-      print("Jūsu elektroauto ir vajadzīga uzlāde!")
-      c = nokrisni() - int(z)
-      print("Uzlādēts!")
-    else:
-      print("Uzpilde nav Jūsu elektroauto nav vajadzīga.")
-      c = a - z
+  if (a - z) < 0:
+    print()
+    print("Jūsu elektroauto ir vajadzīga uzlāde!")
+    c = nokrisni() - int(z)
+    print("Uzlādēts!")
+  else:
+    print("Uzpilde Jūsu elektroauto nav vajadzīga.")
+    c = a - z
   punkti = 0
   skaits = 1
   jautajumi1 = open("jautajumi.csv", "r")
@@ -114,6 +114,7 @@ def cesis():
     else:
       print("Ievadīts nepareizs simbols/atbilde!")
       punkti += 0
+  print("Nākamais kontrolpunkts - Sigulda,",str(39)+"km attālumā.")
   print("Jūs vēl varat nobraukt:", str(c) + "km")
   sleep()
   jautajumi1.close()
@@ -121,7 +122,10 @@ def cesis():
 
 
 def sigulda():
+  print("3. kontrolpunkts - Turaidas pils.")
+  sleep()
   a = cesis()
+  print("Jūs esat nonākuši Turaidas pilī.")
   x1 = open("kontrolpunkti.csv", "r")
   lasa1 = csv.DictReader(x1)
   for rinda in lasa1:
@@ -131,7 +135,7 @@ def sigulda():
     b = nokrisni() - z
     print("Uzlādēts!")
   else:
-    print("Uzpilde nav Jūsu elektroauto nav vajadzīga.")
+    print("Uzpilde Jūsu elektroauto nav vajadzīga.")
     b = a - z
   punkti = 0
   skaits = 1
@@ -157,14 +161,18 @@ def sigulda():
       print("Ievadīts nepareizs simbols/atbilde!")
       punkti += 0
   print("Jūs vēl varat nobraukt:", str(b) + "km.")
+  print("Nākamais kontrolpunkts - Rīga,",str(52)+"km attālumā.")
   sleep()
   jautajumi1.close()
   return b
-#skaititajs()
+
 
 
 def riga():
+  print("4. kontrolpunkts - Brīvības piemineklis.")
+  sleep()
   a = sigulda()
+  print("Jūs esat nonākuši pie Brīvības pieminekļā.")
   x1 = open("kontrolpunkti.csv","r")
   lasa1 = csv.DictReader(x1)
   for rinda in lasa1:
@@ -174,7 +182,7 @@ def riga():
     v = nokrisni() - z
     print("Uzlādēts!")
   else:
-    print("Uzpilde nav Jūsu elektroauto nav vajadzīga.")
+    print("Uzpilde Jūsu elektroauto nav vajadzīga.")
     v = a - z
   punkti = 0
   skaits = 1
@@ -200,13 +208,17 @@ def riga():
       print("Ievadīts nepareizs simbols/atbilde!")
       punkti += 0
   print("Jūs vēl varat nobraukt:", str(v) + "km.")
+  print("Nākamais kontrolpunkts - Jūrmala,",str(80)+"km attālumā.")
   sleep()
   jautajumi1.close()
   return v
 
 
 def jurmala():
+  print("5. kontrolpunkts - Majoru pludmale.")
+  sleep()
   a = riga()
+  print("Jūs esat nonākuši Majoru pludmalē.")
   x1 = open("kontrolpunkti.csv", "r")
   lasa1 = csv.DictReader(x1)
   for rinda in lasa1:
@@ -216,7 +228,7 @@ def jurmala():
       n = nokrisni() - z
       print("Uzlādēts!")
     else:
-      print("Uzpilde nav Jūsu elektroauto nav vajadzīga.")
+      print("Uzpilde Jūsu elektroauto nav vajadzīga.")
       n = a - z
   punkti = 0
   skaits = 1
@@ -242,12 +254,16 @@ def jurmala():
       print("Ievadīts nepareizs simbols/atbilde!")
       punkti += 0
   print("Jūs vēl varat nobraukt:", str(n) + "km.")
+  print("Nākamais kontrolpunkts - Jelgava,",str(57)+"km attālumā.")
   sleep()
   jautajumi1.close()
   return n
 
 def jelgava():
+  print("6. kontrolpunkts - Kurzemes hercogu kapenes.")
+  sleep()
   a = jurmala()
+  print("Jūs esat nonākuši Kurzemes hercogu kapenēs.")
   x1 = open("kontrolpunkti.csv", "r")
   lasa1 = csv.DictReader(x1)
   for rinda in lasa1:
@@ -257,7 +273,7 @@ def jelgava():
     m = nokrisni() - z
     print("Uzlādēts!")
   else:
-    print("Uzpilde nav Jūsu elektroauto nav vajadzīga.")
+    print("Uzpilde Jūsu elektroauto nav vajadzīga.")
     m = a - z
   punkti = 0
   skaits = 1
@@ -283,12 +299,16 @@ def jelgava():
       print("Ievadīts nepareizs simbols/atbilde!")
       punkti += 0
   print("Jūs vēl varat nobraukt:", str(m) + "km.")
+  print("Nākamais kontrolpunkts - Dobele,",str(28)+"km attālumā.")
   sleep()
   jautajumi1.close()
   return m
 
 def dobele():
+  print("7. kontrolpunkts - Dobeles pils.")
+  sleep()
   a = jelgava()
+  print("Jūs esat nonākuši Dobeles pilī.")
   x1 = open("kontrolpunkti.csv", "r")
   lasa1 = csv.DictReader(x1)
   for rinda in lasa1:
@@ -298,7 +318,7 @@ def dobele():
     s = nokrisni() - z
     print("Uzlādēts!")
   else:
-    print("Uzpilde nav Jūsu elektroauto nav vajadzīga.")
+    print("Uzpilde Jūsu elektroauto nav vajadzīga.")
     s = a - z
   punkti = 0
   skaits = 1
@@ -324,12 +344,16 @@ def dobele():
       print("Ievadīts nepareizs simbols/atbilde!")
       punkti += 0
   print("Jūs vēl varat nobraukt:", str(s) + "km.")
+  print("Nākamais kontrolpunkts - Saldus,",str(54)+"km attālumā.")
   sleep()
   jautajumi1.close()
   return s
 
 def saldus():
+  print("8. kontrolpunkts - Kalnsētas parks.")
+  sleep()
   a = dobele()
+  print("Jūs esat nonākuši Kalnsētas parkā.")
   x1 = open("kontrolpunkti.csv", "r")
   lasa1 = csv.DictReader(x1)
   for rinda in lasa1:
@@ -339,7 +363,7 @@ def saldus():
     d = nokrisni() - z
     print("Uzlādēts!")
   else:
-    print("Uzpilde nav Jūsu elektroauto nav vajadzīga.")
+    print("Uzpilde Jūsu elektroauto nav vajadzīga.")
     d = a - z
   punkti = 0
   skaits = 1
@@ -365,12 +389,16 @@ def saldus():
       print("Ievadīts nepareizs simbols/atbilde!")
       punkti += 0
   print("Jūs vēl varat nobraukt:", str(d) + "km.")
+  print("Nākamais kontrolpunkts - Skrunda,",str(32)+"km attālumā.")
   sleep()
   jautajumi1.close()
   return d
 
 def skrunda():
+  print("9. kontrolpunkts - Skrundas lokators.")
+  sleep()
   a = saldus()
+  print("Jūs esat nonākuši pie Skrundas lokatora.")
   x1 = open("kontrolpunkti.csv", "r")
   lasa1 = csv.DictReader(x1)
   for rinda in lasa1:
@@ -380,7 +408,7 @@ def skrunda():
     f = nokrisni() - z
     print("Uzlādēts!")
   else:
-    print("Uzpilde nav Jūsu elektroauto nav vajadzīga.")
+    print("Uzpilde Jūsu elektroauto nav vajadzīga.")
     f = a - z
   punkti = 0
   skaits = 1
@@ -406,12 +434,16 @@ def skrunda():
       print("Ievadīts nepareizs simbols/atbilde!")
       punkti += 0
   print("Jūs vēl varat nobraukt:", str(f) + "km.")
+  print("Nākamais kontrolpunkts - Liepāja,",str(68)+"km attālumā.")
   sleep()
   jautajumi1.close()
   return f
 
 def liepaja():
+  print("10. kontrolpunkts - Koncertzāle 'Lielais dzintars'")
+  sleep()
   a = skrunda()
+  print("Jūs esat nonākuši pie koncertzāles 'Lielais dzintars'.")
   x1 = open("kontrolpunkti.csv", "r")
   lasa1 = csv.DictReader(x1)
   for rinda in lasa1:
@@ -421,7 +453,7 @@ def liepaja():
     g = nokrisni() - z
     print("Uzlādēts!")
   else:
-    print("Uzpilde nav Jūsu elektroauto nav vajadzīga.")
+    print("Uzpilde Jūsu elektroauto nav vajadzīga.")
     g = a - z
   punkti = 0
   skaits = 1
@@ -446,21 +478,18 @@ def liepaja():
     else:
       print("Ievadīts nepareizs simbols/atbilde!")
       punkti += 0
-  print("Jūs esat pabeiguši šo spēli!")
-  f1 = open("saglabats.csv", "w")
+  print("Jūs esat pabeiguši šo spēli! Punkti ir atlikušais kilometru skaits, ko Jūsu elektroauto var nobraukt!")
+  f1 = open("saglabats.csv", "a")
   lauki = ["Vards","Punkti"] 
   raksta = csv.DictWriter(f1, fieldnames=lauki,delimiter=",") 
   for i in range(1):
     x=input(CRED + "Ievadi savu vārdu: " + CEND)
-    y=input(a)
-    raksta.writerow({"Vards": x,"Punkti": y})
+    raksta.writerow({"Vards": x,"Punkti": g})
   jautajumi1.close()
   f1.close()
 
 
 liepaja()
-
-
 def laiks2():
   import time
   seconds = time.time()
@@ -468,6 +497,14 @@ def laiks2():
   print("Laiks, kad beidzāt spēli:", local_time)
 
 
+def izprinte():
+  f1 = open("saglabats.csv", "r")
+  lauki = ["Vards","Punkti"] 
+  raksta = csv.DictReader(f1, fieldnames=lauki,delimiter=",") 
+  for i in range():
+    print(raksta)
+
 
 
 laiks2()
+#izprinte()
